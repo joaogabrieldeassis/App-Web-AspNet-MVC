850e210bf6410e22876ac8e25ab93d0f874fcba5
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-10-08 17:17:35 -0300

    Iniciando a aplicação

--- AppModelo/StoreMvc/Program.cs
+++ AppModelo/StoreMvc/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseMvc(routes =>
+{
+    routes.MapRoute("default","{controller = Home}/{action=Index}/{id?}");
+});
 
 app.Run();
+
+
