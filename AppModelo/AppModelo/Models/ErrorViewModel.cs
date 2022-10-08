1ae9d960e45a2237c4559f7fa1b1c95110734f17
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-10-08 16:38:46 -0300

    Iniciando a aplicação

--- AppModelo/AppModelo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AppModelo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
