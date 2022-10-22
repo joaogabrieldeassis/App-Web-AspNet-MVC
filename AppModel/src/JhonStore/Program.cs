31d01715217bf5991e6c0ec8da471ea799fe7ef4
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-10-22 15:50:25 -0300

    Iniciando Adicionando os arquivos

--- AppModel/src/JhonStore/Program.cs
+++ AppModel/src/JhonStore/Program.cs
@@ -1,7 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
 builder.Services.AddMvc();
 var app = builder.Build();
+app.MapControllers();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
