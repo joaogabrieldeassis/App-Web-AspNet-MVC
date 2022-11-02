c825e0d7a267b794c44d0e3057bc7497169b3fc1
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-02 09:45:18 -0300

    Iniciando o feriado

---
+++ JhonStore/JhonStore/Areas/JoaoTeste/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JhonStore.Areas.JoaoTeste.Controllers
+{
+    [Area("JoaoTeste")]
+    [Route("joao")]
+    public class TesteController : Controller
+    {
+
+        [Route("list/{id?}/{nome?}")]
+        public IActionResult Index(int id, string nome)
+        {
+           return View();
+        }
+    }
+}
--- JhonStore/JhonStore/Program.cs
+++ JhonStore/JhonStore/Program.cs
@@ -4,13 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.Configure<RazorViewEngineOptions>(options =>
-{
-    options.AreaViewLocationFormats.Clear();
-    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
-    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
-    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
-});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
