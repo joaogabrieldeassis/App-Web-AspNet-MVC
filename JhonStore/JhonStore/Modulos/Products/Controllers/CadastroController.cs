7681485707bb32c893c3ca5f3e32b9bf9eb0da14
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-02 08:10:42 -0300

    Iniciando o feriado

--- JhonStore/JhonStore/Modulos/Products/Controllers/CadastroController.cs
+++ JhonStore/JhonStore/Modulos/Products/Controllers/CadastroController.cs
@@ -3,8 +3,10 @@
 namespace JhonStore.Modulos.Products.Controllers
 {
     [Area("Products")]
+    [Route("products")]
     public class CadastroController : Controller
     {
+        [Route("list")]
         public IActionResult Index()
         {
             return View();
---
+++ JhonStore/JhonStore/Modulos/Vendas/Controllers/PedidosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JhonStore.Modulos.Vendas.Controllers
+{
+    [Area("Vendas")]
+    [Route("pedidos")]
+    public class PedidosController : Controller
+    {
+        [Route("listar")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
