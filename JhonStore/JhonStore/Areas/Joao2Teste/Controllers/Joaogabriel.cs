978a572c724764b368e888db1b826969797e4ca7
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-03 07:42:21 -0300

    Iniciando o dia

---
+++ JhonStore/JhonStore/Areas/Joao2Teste/Controllers/Joaogabriel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JhonStore.Areas.Joao2Teste.Controllers
+{
+    [Area("Joao2Teste")]
+    [Route("cadastro")]
+    public class Joaogabriel : Controller
+    {
+        [Route("cadastre")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
