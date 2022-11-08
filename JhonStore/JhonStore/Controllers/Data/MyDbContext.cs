2540ff5c290813437e9fbdbd968bac6bd90c4cec
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-08 12:43:55 -0300

    Configurando o EfCore

--- JhonStore/JhonStore/Controllers/Data/MyDbContext.cs
+++ JhonStore/JhonStore/Controllers/Data/MyDbContext.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JhonStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JhonStore.Controllers.Data
 {
     public class MyDbContext : DbContext
     {
         private readonly ILoggerFactory _logger = LoggerFactory.Create(x => x.AddConsole());
+
+        public DbSet<Aluno> Alunos{ get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             
         options.UseLoggerFactory(_logger)
                 .EnableSensitiveDataLogging()
-                .UseSqlServer("Server=localhost,1433;Database=JhonStore;User ID=sa;Password=;Trusted_Connection=False; TrustServerCertificate=True;");
+                .UseSqlServer("Server=localhost,1433;Database=JhonStore;User ID=sa;Password=@#$;Trusted_Connection=False; TrustServerCertificate=True;");
         }
     }
 }
---
+++ JhonStore/JhonStore/Controllers/TesteCrudController.cs
@@ -0,0 +1,29 @@
+﻿using JhonStore.Controllers.Data;
+using JhonStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JhonStore.Controllers
+{
+    public class TesteCrudController : Controller
+    {
+        private readonly MyDbContext _context;
+        public TesteCrudController(MyDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Jesus",
+                Email = "joao",
+                DataDeNascimento = DateTime.Now
+            };
+            _context.Alunos.Add(aluno);
+            _context.SaveChanges();
+
+            var studantsTwo = _context.Alunos.Find(aluno.Id);
+            return View();
+        }
+    }
+}
