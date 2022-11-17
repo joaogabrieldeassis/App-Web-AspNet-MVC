c9b8d227023cec11d9dec2d12dda28b242af1d1a
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-17 13:20:20 -0300

    Resolvendo o problema

---
+++ WebApplication2/WebApplication2/Config/IdentityConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace WebApplication2.Config
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection ResolveDependencies(IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("PodeExcluir", policy => policy.RequireClaim("PodeExcluir"));
+            });
+            return services;
+        }
+    }
+}
--- WebApplication2/WebApplication2/Program.cs
+++ WebApplication2/WebApplication2/Program.cs
@@ -5,26 +5,15 @@ using WebApplication2.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("WebApplication2ContextConnection") ?? throw new InvalidOperationException("Connection string 'WebApplication2ContextConnection' not found.");
 
-builder.Services.AddDbContext<WebApplication2Context>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    
-    .AddEntityFrameworkStores<WebApplication2Context>();
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("PodeExcluir", policy => policy.RequireClaim("PodeExcluir"));
-});
+AddIdentityConfig(builder);
+ConfigurationIdentity(builder);
 builder.Services.AddRazorPages();
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -39,3 +28,23 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void ConfigurationDependenciInjection(WebApplicationBuilder builder)
+{
+
+}
+void ConfigurationIdentity(WebApplicationBuilder builder)
+{
+    builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("PodeExcluir", policy => policy.RequireClaim("PodeExcluir"));
+    });
+}
+void AddIdentityConfig(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<WebApplication2Context>(options =>
+    options.UseSqlServer(connectionString));
+
+    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<WebApplication2Context>();
+}
