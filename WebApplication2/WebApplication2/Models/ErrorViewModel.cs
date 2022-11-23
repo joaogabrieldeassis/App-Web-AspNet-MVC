bc64421f03a67138364664baba6e587e0dfd5ed5
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-23 14:23:25 -0300

    Iniciando a aplicação

--- WebApplication2/WebApplication2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication2.Models
-{
-    public class ErrorViewModel
-    {
-        public int ErrorCode { get; set; }
-        public string Titulo { get; set; }
-        public string Message { get; set; }
-    }
-}
