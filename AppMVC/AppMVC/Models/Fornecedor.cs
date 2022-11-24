dd2eb12437c22c1b3b0111e99db8a9db5229974d
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-24 12:49:40 -0300

    Criando os Objetos

--- AppMVC/AppMVC/Models/Fornecedor.cs
+++ AppMVC/AppMVC/Models/Fornecedor.cs
@@ -1,6 +1,13 @@
-﻿namespace AppMVC.Models
+﻿using AppMVC.Models.Enuns;
+
+namespace AppMVC.Models
 {
     public class Fornecedor : Entity
     {
+        public string Nome { get; set; }
+        public string Documento{ get; set; }
+        public TipoDeFornecedor TipoDeFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
     }
 }
