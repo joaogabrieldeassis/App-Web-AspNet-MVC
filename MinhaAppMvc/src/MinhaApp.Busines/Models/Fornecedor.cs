b7c9c48f05f881f621b576a0d3a285b540aac7a4
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-11-28 13:42:49 -0300

    Subindo a aplicação

---
+++ MinhaAppMvc/src/MinhaApp.Busines/Models/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿using MinhaApp.Busines.Models.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApp.Busines.Models
+{
+    public class Fornecedor
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoDoFornecedor TipoDoFornecedor { get; set; }        
+        public bool Ativo { get; set; }
+        public Endereco Endereco { get; set; }
+        public IEnumerator<Produto> Produtos { get; set; }
+    }
+}
