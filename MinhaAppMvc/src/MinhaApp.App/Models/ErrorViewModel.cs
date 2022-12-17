597160cd2f4a6774745cc485224a4c4087685768
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-17 14:22:34 -0300

    Iniciando a aplicação

---
+++ MinhaAppMvc/src/MinhaApp.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinhaApp.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
