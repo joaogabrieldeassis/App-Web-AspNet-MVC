d70d8d5ff21365cd698a5b57398612dc2548ad6f
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-20 07:44:15 -0300

    Criando a camada de acesso a dados

---
+++ MinhaAppSolution/src/Dev.Bussines/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using MinhaAp.Busines.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev.Bussines.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+    }
+}
