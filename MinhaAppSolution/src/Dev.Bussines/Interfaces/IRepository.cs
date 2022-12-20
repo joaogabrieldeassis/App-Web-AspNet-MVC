fd5ec0c0fbf70230742898f9ab80ad233eba55dd
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-20 08:02:16 -0300

    Criando a camada de acesso a dados

--- MinhaAppSolution/src/Dev.Bussines/Interfaces/IRepository.cs
+++ MinhaAppSolution/src/Dev.Bussines/Interfaces/IRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,12 @@ namespace Dev.Bussines.Interfaces
 {
     public interface IRepository<TEntity> : IDisposable where TEntity : Entity
     {
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(TEntity entity);
+        Task Atualizar(TEntity entity);
+        Task<List<TEntity>> ObterTodos();
+        Task Deletar(Guid id);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
     }
 }
