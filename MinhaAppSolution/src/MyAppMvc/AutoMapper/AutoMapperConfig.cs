3701b8bfc87c7b636d5e0845e1182e2bdb7938cc
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-21 08:31:13 -0300

    Criando as views models

---
+++ MinhaAppSolution/src/MyAppMvc/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Dev.AppMvc.ViewModels;
+using MinhaAp.Busines.Models;
+
+namespace Dev.AppMvc.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+
+        }
+    }
+}
