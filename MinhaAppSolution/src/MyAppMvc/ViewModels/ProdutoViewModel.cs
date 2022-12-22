8278ed1fdae0f653286ee18a7a45326b32de59fe
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-22 06:58:56 -0300

    Criando as views models

--- MinhaAppSolution/src/MyAppMvc/ViewModels/ProdutoViewModel.cs
+++ MinhaAppSolution/src/MyAppMvc/ViewModels/ProdutoViewModel.cs
@@ -8,19 +8,28 @@ namespace Dev.AppMvc.ViewModels
     {
         [Key]
         public Guid Id { get; set; }
+
         [Required(ErrorMessage = "O campo {0} é obrigatorio")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracters", MinimumLength = 2)]       
         public string Nome { get; set; }
+
+        public Guid FornecedorId { get; set; }
+
         [DisplayName("Descrição")]
         [Required(ErrorMessage = "O campo {0} é obrigatorio")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracters", MinimumLength = 2)]
         public string Descricao { get; set; }
+
         public IFormFile ImagemUplade { get; set; }
+
         public string Imagem { get; set; }
         [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+
         public decimal Valor { get; set; }
         [ScaffoldColumn(false)]
+
         public DateTime DataDeCadastro { get; set; }
+
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
         public FornecedorViewModel Fornecedor { get; set; }
