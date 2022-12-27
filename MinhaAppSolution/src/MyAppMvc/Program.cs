d717bc58932741e747503a46a407a04b7b3b5d6e
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-12-27 07:51:47 -0300

    Criando a controller de Produtos

--- MinhaAppSolution/src/MyAppMvc/Program.cs
+++ MinhaAppSolution/src/MyAppMvc/Program.cs
@@ -10,12 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
         .AddEntityFrameworkStores<MeuDbContext>();
 
 builder.Services.AddDbContext<MeuDbContext>(options =>
