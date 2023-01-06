1b000b377afc1603410b3192916ac35787776bb7
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2023-01-06 12:54:36 -0300

    Validando as entidades

--- MinhaAppSolution/src/Dev.Bussines/Model/Produto.cs
+++ MinhaAppSolution/src/Dev.Bussines/Model/Produto.cs
@@ -11,7 +11,7 @@ namespace MinhaAp.Busines.Models
         public Guid FornecedorId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public string Imagem { get; set; }
+        public string? Imagem { get; set; }
         public decimal Valor { get; set; }
         public DateTime DataDeCadastro { get; set; }
         public bool Ativo { get; set; }
